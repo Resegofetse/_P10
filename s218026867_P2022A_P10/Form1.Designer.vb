b5cc8ce5dd3cc709489a26cff3344e6ec5e61428
resegofetse <resegofetsepanana11@gmail.com>
2022-11-16 13:41:57 +0200

    Add project files.

---
+++ s218026867_P2022A_P10/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_departments
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_input = New System.Windows.Forms.Button()
+        Me.btn_average = New System.Windows.Forms.Button()
+        Me.grd_departments = New UJGrid.UJGrid()
+        Me.btn_Rating = New System.Windows.Forms.Button()
+        Me.btn_highest = New System.Windows.Forms.Button()
+        Me.txt_rating = New System.Windows.Forms.TextBox()
+        Me.txt_name = New System.Windows.Forms.TextBox()
+        Me.lbl_name = New System.Windows.Forms.Label()
+        Me.lbl_rating = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_input
+        '
+        Me.btn_input.Location = New System.Drawing.Point(12, 12)
+        Me.btn_input.Name = "btn_input"
+        Me.btn_input.Size = New System.Drawing.Size(75, 23)
+        Me.btn_input.TabIndex = 0
+        Me.btn_input.Text = "Input"
+        Me.btn_input.UseVisualStyleBackColor = True
+        '
+        'btn_average
+        '
+        Me.btn_average.Location = New System.Drawing.Point(12, 61)
+        Me.btn_average.Name = "btn_average"
+        Me.btn_average.Size = New System.Drawing.Size(75, 23)
+        Me.btn_average.TabIndex = 2
+        Me.btn_average.Text = "Average"
+        Me.btn_average.UseVisualStyleBackColor = True
+        '
+        'grd_departments
+        '
+        Me.grd_departments.FixedCols = 1
+        Me.grd_departments.FixedRows = 1
+        Me.grd_departments.Location = New System.Drawing.Point(112, -3)
+        Me.grd_departments.Name = "grd_departments"
+        Me.grd_departments.Scrollbars = System.Windows.Forms.ScrollBars.Both
+        Me.grd_departments.Size = New System.Drawing.Size(635, 139)
+        Me.grd_departments.TabIndex = 3
+        '
+        'btn_Rating
+        '
+        Me.btn_Rating.Location = New System.Drawing.Point(12, 113)
+        Me.btn_Rating.Name = "btn_Rating"
+        Me.btn_Rating.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Rating.TabIndex = 4
+        Me.btn_Rating.Text = "Rating"
+        Me.btn_Rating.UseVisualStyleBackColor = True
+        '
+        'btn_highest
+        '
+        Me.btn_highest.Location = New System.Drawing.Point(12, 172)
+        Me.btn_highest.Name = "btn_highest"
+        Me.btn_highest.Size = New System.Drawing.Size(75, 23)
+        Me.btn_highest.TabIndex = 5
+        Me.btn_highest.Text = "Highest"
+        Me.btn_highest.UseVisualStyleBackColor = True
+        '
+        'txt_rating
+        '
+        Me.txt_rating.Location = New System.Drawing.Point(459, 179)
+        Me.txt_rating.Name = "txt_rating"
+        Me.txt_rating.Size = New System.Drawing.Size(100, 20)
+        Me.txt_rating.TabIndex = 6
+        '
+        'txt_name
+        '
+        Me.txt_name.Location = New System.Drawing.Point(220, 179)
+        Me.txt_name.Name = "txt_name"
+        Me.txt_name.Size = New System.Drawing.Size(100, 20)
+        Me.txt_name.TabIndex = 7
+        '
+        'lbl_name
+        '
+        Me.lbl_name.AutoSize = True
+        Me.lbl_name.Location = New System.Drawing.Point(139, 182)
+        Me.lbl_name.Name = "lbl_name"
+        Me.lbl_name.Size = New System.Drawing.Size(38, 13)
+        Me.lbl_name.TabIndex = 8
+        Me.lbl_name.Text = "Name:"
+        '
+        'lbl_rating
+        '
+        Me.lbl_rating.AutoSize = True
+        Me.lbl_rating.Location = New System.Drawing.Point(373, 186)
+        Me.lbl_rating.Name = "lbl_rating"
+        Me.lbl_rating.Size = New System.Drawing.Size(41, 13)
+        Me.lbl_rating.TabIndex = 9
+        Me.lbl_rating.Text = "Rating:"
+        '
+        'frm_departments
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbl_rating)
+        Me.Controls.Add(Me.lbl_name)
+        Me.Controls.Add(Me.txt_name)
+        Me.Controls.Add(Me.txt_rating)
+        Me.Controls.Add(Me.btn_highest)
+        Me.Controls.Add(Me.btn_Rating)
+        Me.Controls.Add(Me.grd_departments)
+        Me.Controls.Add(Me.btn_average)
+        Me.Controls.Add(Me.btn_input)
+        Me.Name = "frm_departments"
+        Me.Text = "Departments"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_input As Button
+    Friend WithEvents btn_average As Button
+    Friend WithEvents grd_departments As UJGrid.UJGrid
+    Friend WithEvents btn_Rating As Button
+    Friend WithEvents btn_highest As Button
+    Friend WithEvents txt_rating As TextBox
+    Friend WithEvents txt_name As TextBox
+    Friend WithEvents lbl_name As Label
+    Friend WithEvents lbl_rating As Label
+End Class
---
+++ s218026867_P2022A_P10/Form1.vb
@@ -0,0 +1,147 @@
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+
+Public Class frm_departments
+    'record structures and other variables
+    Private Structure DepartmentRec
+        Public Name As String
+        Public NumStudents As Integer
+        Public Average As Double
+        Public Rating As Integer
+    End Structure
+    Private Structure ModuleRec
+        Public ModuleCode As String
+        Public sizeupload() As Double       'nW
+        Public Total As Double
+    End Structure
+    Private Departments() As DepartmentRec  'nD
+    Private Modules() As ModuleRec          'nM
+    Private nD, nM, nW As Integer
+
+    'subroutines and functions 
+
+    Private Sub PT(ByVal r As Integer, ByVal c As Integer, ByVal t As String)
+        grd_departments.Row = r
+        grd_departments.Col = c
+        grd_departments.Text = t
+    End Sub
+
+
+
+    Private Sub Setup()
+        'input from user
+        nD = CInt(InputBox("Enter Number of Departments"))
+        nM = CInt(InputBox("Enter the number of Modules"))
+        nW = CInt(InputBox("Enter the number of weeks"))
+
+        'resize arrays
+        ReDim Departments(nD)
+        For d As Integer = 1 To nD
+            ReDim Modules(nM)
+        Next
+        For i As Integer = 1 To nM
+            ReDim Modules(i).sizeupload(nW)
+        Next
+        'resize uj grid
+        grd_departments.Rows = nD + 1
+        grd_departments.Cols = nM + 1 + 2
+
+        'label uj grid
+        For d As Integer = 1 To nD
+            PT(d, 0, "Department #" & CStr(d))
+        Next
+        For m As Integer = 1 To nM
+            PT(0, m, "Module #" & CStr(m))
+        Next
+
+        PT(0, nM + 1, "Average")
+        PT(0, nM + 2, "Rating")
+
+    End Sub
+
+    Private Function ConvertG(ByVal size As Double) As Double
+
+        Return size / 1000
+    End Function
+
+    Private Function CalcAve(ByVal total As Double, ByVal num As Integer) As Double
+        Return total / num
+    End Function
+    Private Function DetermineRating(ByVal rating As Double) As String
+        Dim response As String
+        If rating >= 0 And rating < 0.75 Then
+            response = "1"
+        Else
+            If rating >= 0.75 And rating < 2.5 Then
+                response = "2"
+            Else
+                If rating >= 2.5 Then
+                    response = "3"
+                End If
+            End If
+        End If
+        Return response
+    End Function
+
+    'input from the user
+    Private Sub btn_input_Click(sender As Object, e As EventArgs) Handles btn_input.Click
+        Setup()
+        For d As Integer = 1 To nD
+            Departments(d).Name = InputBox("Enter Name of the Department #" & CStr(d))
+            Departments(d).NumStudents = CInt(InputBox("Enter number of students in the department #") & CStr(d))
+            For m As Integer = 1 To nM
+                Modules(m).Total = 0
+                Modules(m).ModuleCode = InputBox("Enter Module code for module #" & CStr(m))
+                For w As Integer = 1 To nW
+                    Modules(m).sizeupload(w) = CDbl(InputBox("Enter Size of File upload for module #" & CStr(m) & "in week # " & CStr(w)))
+                    Modules(m).Total = Modules(m).Total + Modules(m).sizeupload(w)
+
+                Next w
+                PT(d, m, CStr(Modules(m).Total))
+            Next m
+        Next d
+    End Sub
+
+
+    'Calculating average
+    Private Sub btn_average_Click(sender As Object, e As EventArgs) Handles btn_average.Click
+        Dim Total As Double
+        For d As Integer = 1 To nD
+            Total = 0
+            For m As Integer = 1 To nM
+
+                Total += Modules(m).Total
+            Next m
+            Departments(d).Average = CalcAve(Total, nM)
+            'display
+            PT(d, nM + 1, Format(Departments(d).Average, "0.0000"))
+            Next d
+
+    End Sub
+    'rating and display
+    Private Sub btn_Rating_Click(sender As Object, e As EventArgs) Handles btn_Rating.Click
+        Dim Avg As Double = 0
+        For d As Integer = 1 To nD
+            Avg = ConvertG(Departments(d).Average)
+            Departments(d).Rating = CInt(DetermineRating(Avg))
+            PT(d, nM + 2, CStr(Departments(d).Rating))
+        Next d
+
+    End Sub
+    'highest size upload module and the displays 
+    Private Sub btn_highest_Click(sender As Object, e As EventArgs) Handles btn_highest.Click
+        Dim max As Double
+        For d As Integer = 1 To nD
+            max = Departments(1).Average
+            If Departments(d).Average >= max Then
+                max = Departments(d).Average
+                txt_name.Text = Departments(d).Name
+                txt_rating.Text = CStr(Departments(d).Rating)
+            End If
+        Next d
+
+    End Sub
+
+
+End Class
